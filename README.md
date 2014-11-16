82795004c319c0a501b58460b8d39b1953e23597
Yusuf Doru <doru.yusuf@gmail.com>
2014-11-16 13:48:12 +0200

    uploaded 16.11.14

---
+++ README.md
@@ -0,0 +1,1 @@
+Domain Availability - C#, WPF
---
+++ domainSorgulama/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Threading;
+
+
+namespace domainSorgulama
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        WebClient wClient;
+        string sourceUrl = "";
+        string source = "";
+        string desen = "alÄ±nmamÄ±ÅŸ.";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            wClient = new WebClient();
+            wClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(wClient_DownloadStringCompleted);
+        }
+
+        void wClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                source = e.Result;
+                bool varmi = Regex.IsMatch(source, desen);
+                if (varmi)
+                    lblSonuc.Content = "Daha önce alınmamış.";
+                else
+                    lblSonuc.Content = "Daha önce alınmış.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+            
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sourceUrl = "http://kimindir.com/" + txtDomain.Text;
+                wClient.DownloadStringAsync(new Uri(sourceUrl));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+
+        }
+    }
+}
